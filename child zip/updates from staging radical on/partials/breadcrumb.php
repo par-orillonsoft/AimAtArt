9b6e65b7590d3b916a5e75f574bd3c7defed9dcb
bochelord <bochelord@gmail.com>
2015-02-23 20:41:07 +0100

    another change. Now with article.php in play

--- child zip/updates from staging radical on/partials/breadcrumb.php
+++ child zip/updates from staging radical on/partials/breadcrumb.php
@@ -97,10 +97,7 @@ else :
                 </div>
                 <div class="pre-shape-form"></div>
                 <div class="post-shape-form"></div>
-                <div class="blog-button-back">
-                	<a class="blog-button-back btn-color btn-color-1d big-size  nav-to tp-button red-fill " href="http://radicalgraphics.com/AAA/?page_id=659">
-					naar inzichten</a>
-				</div>
+                
             </div>
         </div>
 <?php endif; ?> 
