d8f2fb2cd9a9fa36dced50e772f0a0e0e7de1fc4
bochelord <bochelord@gmail.com>
2015-02-23 20:18:22 +0100

    fix for breadcumbs naar inzichten link back

--- child zip/updates from staging radical on/partials/breadcrumb.php
+++ child zip/updates from staging radical on/partials/breadcrumb.php
@@ -51,6 +51,8 @@ if(isset($page_setting_activate) && $page_setting_activate=='on') :
                 </div>
 				<div class="pre-shape-form"></div>
                 <div class="post-shape-form"></div>
+                <a class="btn-color btn-color-1d big-size  nav-to tp-button red-fill " href="http://radicalgraphics.com/AAA/?page_id=659">
+				naar inzichten</a>
             </div>
         </div>
 
@@ -95,7 +97,8 @@ else :
                 </div>
                 <div class="pre-shape-form"></div>
                 <div class="post-shape-form"></div>
-                
+                <a class="btn-color btn-color-1d big-size  nav-to tp-button red-fill " href="http://radicalgraphics.com/AAA/?page_id=659">
+				naar inzichten</a>
             </div>
         </div>
 <?php endif; ?> 
