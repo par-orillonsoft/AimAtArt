3109c87113bc44064be34b0978eaea0283f67fbf
bochelord <bochelord@gmail.com>
2015-02-23 20:23:25 +0100

    fix again this time breadcrumbs and blog

--- child zip/updates from staging radical on/partials/breadcrumb.php
+++ child zip/updates from staging radical on/partials/breadcrumb.php
@@ -97,8 +97,10 @@ else :
                 </div>
                 <div class="pre-shape-form"></div>
                 <div class="post-shape-form"></div>
-                <a class="btn-color btn-color-1d big-size  nav-to tp-button red-fill " href="http://radicalgraphics.com/AAA/?page_id=659">
-				naar inzichten</a>
+                <div class="align-center">
+                	<a class="btn-color btn-color-1d big-size  nav-to tp-button red-fill " href="http://radicalgraphics.com/AAA/?page_id=659">
+					naar inzichten</a>
+				</div>
             </div>
         </div>
 <?php endif; ?> 
